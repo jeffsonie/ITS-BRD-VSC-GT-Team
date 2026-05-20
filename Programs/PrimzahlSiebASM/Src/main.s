43265dff07c4baf6b4c92d022930845f6e9450f2
DJ <dennis.weazle5968@gmail.com>
2026-05-20 17:14:20 +0200

    main.s Comments Added

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,11 +1,10 @@
 ;******************** (C) COPYRIGHT HAW-Hamburg ********************************
-;* File Name          : main.s
-;* Author             : Silke Behn	
+;* File Name          : PrimzahlSieb
+;* Author             : Arash Ghaboos, Dennis Jarmolinski 	
 ;* Version            : V1.0
-;* Date               : 01.06.2021
-;* Description        : This is a simple main.
-;					  :
-;					  : Replace this main with yours.
+;* Date               : 20.05.2026
+;* Description        : Ein Primzahl Sieb was alle Zahlen "aussiebt"
+;					  : und nur die Primzahlen im Speicher ablegt.
 ;
 ;*******************************************************************************
     EXTERN initITSboard
@@ -18,6 +17,9 @@
 ;********************************************
 	
 	AREA MyData, DATA, align = 2
+
+	; Platzhalter für die sieb_liste (1000 Bytes)
+    ; Platzhalter für die ergebnis_liste
 	
 	    GLOBAL text
 DEFAULT_BRIGHTNESS DCW  800
@@ -42,9 +44,30 @@ main	PROC
 		bl GUI_init
 		mov r0, #0x00
 ;		bl TP_Init
-		
 		LDR	r0,=text
         BL  lcdPrintS
+
+        ; ERSTER SCHRITT
+        ; Basisadresse von der Sieb Liste in ein Register laden
+        ; Die ganze Liste mit Einsen füllen
+
+        ; SIEBEN
+        ; äussere Schleife:
+        ; Prüfen, ob i*i grösser als 1000 ist. Wenn ja -> Abbruch
+        ; Wert an der Stelle i laden. Ist er 0? -> zum nächsten Durchlauf springen
+        ; 
+        ; innere Schleife (Vielfache streichen):
+        ; j = i*i berechnen
+        ; Ist j groesser als 1000? -> innere Schleife abbrechen
+        ; An der Stelle j eine 0 reinschreiben
+        ; j = j + i rechnen
+        ; Wiederholen
+
+        ; 3. ABSPEICHERN
+        ; Basisadresse vom Sieb Liste laden
+        ; Basisadresse vom Ergebnis Liste laden
+        ; Plätze 2 bis 1000 durchgehen
+        ; Wenn an der Stelle eine 1 steht, den Platz in der Ergebnis Liste speichern
 
 forever	b	forever		; nowhere to retun if main ends		
 		ENDP
