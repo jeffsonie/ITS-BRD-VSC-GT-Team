405de34d496c768f07780a95b25ed7ea24cef602
DJ <dennis.weazle5968@gmail.com>
2026-05-28 11:04:48 +0200

    Added Aufgabe 5.2

--- Programs/PrimzahlSiebASM/README.md
+++ Programs/PrimzahlSiebASM/README.md
@@ -1,32 +1,30 @@
 Primzahlsieb des Eratosthenes
 
 Beschreibung
+Sieb des Eratosthenes in ARM-Assembler. Findet alle Primzahlen von 2 bis 1000
+und markiert sie in sieb_liste (Index = Zahl, Wert 1 = Primzahl, 0 = keine).
 
-Implementierung des Siebs des Eratosthenes in ARM-Assembler.  
-Ermittelt alle Primzahlen von 2 bis 1000 und speichert das Ergebnis in "sieb_liste".
+Ablauf
 
-Programmstruktur
+Schritt 1 Initialisierung (for_01)
+Alle Einträge auf 1 setzen (erstmal alle als Primzahl annehmen).
+Index 0 und 1 danach auf 0, da keine Primzahlen.
 
-Schritt 1 Initialisierung
-- Zählschleife (for_01): Setzt alle Einträge der "sieb_liste" (Index 0–1000) auf "1".  
-- Anschließend werden Index 0 und 1 manuell auf "0" gesetzt (keine Primzahlen).
+Schritt 2 Sieb (while_01 / while_02)
+- Äußere Schleife: i läuft ab 2, solange i*i ≤ 1000
+- Ist sieb_liste[i] noch 1, werden alle Vielfachen von i (ab i*i) auf 0 gesetzt
 
-Schritt 2 – Sieb (Eratosthenes)
-- Äußere Schleife (while_01): läuft solange "i*i ≤ 1000", prüft ob "sieb_liste[i] == 1"
-- Innere Schleife (while_02): streicht alle Vielfachen von "i" ab "i*i" aus der Liste
+Schritt 3 Abspeichern
+Noch nicht gefordert. Speicher (ergebnis_liste) ist reserviert.
 
-Schritt 3 – Abspeichern
-Noch nicht gefordert. Speicher (ergebnis_liste) ist bereits reserviert.
+Register
+ Reg / Bedeutung 
+-----/-----------
+  R0 / Basisadresse sieb_liste 
+  R1 / i (Schritt 1) 
+  R4 / i (Schritt 2, äußere Schleife) 
+  R5 / j (innere Schleife) 
+  R6 / Hilfsregister 
 
-Kontrollstrukturen
-
-Äußere Schleife (while_01) / for i=2; i*i≤1000; i++
-- Kopf: "MUL R6,R4,R4" -> "CMP R6,#1000" -> "BGT endwhile_01"
-- Körper: "do_while_01"
-
-Flalunterscheidung (if_01) / if sieb_liste[i] != 0
-- LDRB R6,[R0,R4] -> CMP R6,#0 -> BEQ endif_01
-
-Innere Schleife (while_02) / while j<=1000
-- Kopf: CMP R5,#1000 -> BGT endwhile_02
-- Körper: do_while_02
+Hinweis
+Vergleiche arbeiten im unsigned-Bereich (BHS / BHI), da alle Werte größer gleich 0 sind.
--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,124 +1,83 @@
 ;******************** (C) COPYRIGHT HAW-Hamburg ********************************
 ;* File Name          : PrimzahlSieb
 ;* Author             : Arash Ghaboos, Dennis Jarmolinski
-;* Version            : V1.0
+;* Version            : V3.0
 ;* Date               : 27.05.2026
-;* Description        : Primzahlsieb des Eratosthenes fuer 2 bis 1000
+;* Description        : Primzahlsieb des Eratosthenes für 2 bis 1000
 ;*******************************************************************************
 
     EXTERN initITSboard
 
 ;============================================================
-; Datensegment - hier legen wir unsere Variablen im RAM ab
+; Datensegment
 ;============================================================
     AREA MyData, DATA, ALIGN=2
 
-; sieb_liste hat 1001 Einträge (Index 0 bis 1000)
-; 1 bedeutet: diese Zahl ist eine Primzahl
-; 0 bedeutet: diese Zahl ist KEINE Primzahl
-sieb_liste      FILL 1001       ; 1001 Bytes fuer das Sieb
-
-; hier kommen später die gefundenen Primzahlen rein (Schritt 3)
-ergebnis_liste  FILL 1000       ; 1000 Bytes fuer die Ergebnisse
+sieb_liste      FILL 1001       ; Index 0..1000: 1 = Primzahl, 0 = keine
+ergebnis_liste  FILL 1000       ; für Schritt 3
 
 ;============================================================
-; Codesegment - hier steht unser Programm
+; Codesegment
 ;============================================================
     AREA |.text|, CODE, READONLY, ALIGN=3
 
     EXPORT main [CODE]
 
 main    PROC
-        ; Board initialisieren, das muss immer als erstes gemacht werden
-        BL   initITSboard
+        BL   initITSboard       ; Board init
+        LDR  R0, =sieb_liste    ; R0 = Basisadresse
 
-        ; Basisadresse von sieb_liste in R0 laden
-        ; R0 bleibt fuer den gesamten Algorithmus die Basisadresse
-        LDR  R0, =sieb_liste
+        ; ===== SCHRITT 1: alle Einträge auf 1 setzen =====
+        MOV  R3, #1
 
-        ; ============================================================
-        ; SCHRITT 1: INITIALISIERUNG
-        ; Wir setzen alle Einträge in sieb_liste auf 1
-        ; Am Anfang gehen wir davon aus, dass alle Zahlen Primzahlen sind
-        ; Nicht-Primzahlen werden später auf 0 gesetzt
-        ; ============================================================
-
-        MOV  R3, #1             ; R3 = 1, das ist der Wert den wir reinschreiben
-
-for_01                          ; Schleife starten: i = 0
-        MOV  R1, #0             ; R1 = i = 0 (Laufvariable, faengt bei 0 an)
-        MOV  R2, #1001        ; R2 = Endwert, Schleife läuft bis i < 1001
-
+for_01
+        MOV  R1, #0             ; i = 0
+        MOV  R2, #1001          ; Endwert
 until_01
-        CMP  R1, R2             ; ist i schon >= 1001?
-        BGE  enddo_01           ; ja -> Schleife beenden
-
+        CMP  R1, R2             ; i >= 1001? (unsigned)
+        BHS  enddo_01           ; HS = Higher or Same (>=)
 do_01
         STRB R3, [R0, R1]       ; sieb_liste[i] = 1
-
 step_01
-        ADD  R1, R1, #1         ; i = i + 1
-        B    until_01           ; zurück zur Prüfung
+        ADD  R1, R1, #1         ; i++
+        B    until_01
 enddo_01
 
-        ; 0 und 1 sind keine Primzahlen
-        ; deshalb setzen wir die beiden manuell auf 0
-        MOV  R3, #0
-        STRB R3, [R0, #0]       ; sieb_liste[0] = 0
-        STRB R3, [R0, #1]       ; sieb_liste[1] = 0
+        MOV  R3, #0             ; 0 und 1 sind keine Primzahlen
+        STRB R3, [R0, #0]
+        STRB R3, [R0, #1]
 
-        ; ============================================================
-        ; SCHRITT 2: DAS SIEB
-        ; Wir suchen alle Vielfachen von Primzahlen und streichen sie
-        ; Äussere Schleife: i läuft von 2 bis i*i > 1000
-        ; Innere Schleife:   alle Vielfachen von i ab i*i auf 0 setzen
-        ; ============================================================
-
-        MOV  R4, #2             ; R4 = i = 2, wir fangen bei 2 an
+        ; ===== SCHRITT 2: Vielfache streichen =====
+        MOV  R4, #2             ; i = 2
 
 while_01
-        ; Abbruchbedingung: wenn i*i grösser als 1000 ist, sind wir fertig
-        MUL  R6, R4, R4         ; R6 = i * i
-        CMP  R6, #1000        ; ist i*i > 1000?
-        BGT  endwhile_01        ; ja -> äussere Schleife beenden
-
+        MUL  R6, R4, R4         ; i*i
+        CMP  R6, #1000          ; i*i > 1000? (unsigned)
+        BHI  endwhile_01        ; HI = Higher (>)
 do_while_01
-                                ; prüfen ob sieb_liste[i] noch eine Primzahl ist
-                                ; wenn nicht, müssen wir nichts streichen
 if_01
-        LDRB R6, [R0, R4]       ; R6 = sieb_liste[i]
-        CMP  R6, #0             ; ist sieb_liste[i] == 0 (keine Primzahl)?
-        BEQ  endif_01           ; ja -> ueberspringen, nichts zu tun
-
+        LDRB R6, [R0, R4]       ; sieb_liste[i]
+        CMP  R6, #0             ; keine Primzahl?
+        BEQ  endif_01           ; dann überspringen
 then_01
-                                ; i ist eine Primzahl, also alle Vielfachen von i streichen
-                                ; wir fangen bei i*i an, weil kleinere Vielfache schon erledigt sind
-        MUL  R5, R4, R4         ; R5 = j = i * i (Startwert der inneren Schleife)
-
+        MUL  R5, R4, R4         ; j = i*i
 while_02
-        CMP  R5, #1000        ; ist j > 1000?
-        BGT  endwhile_02        ; ja -> innere Schleife beenden
-
+        CMP  R5, #1000          ; j > 1000? (unsigned)
+        BHI  endwhile_02        ; HI = Higher (>)
 do_while_02
-        ; dieses Vielfache ist keine Primzahl, auf 0 setzen
         MOV  R6, #0
         STRB R6, [R0, R5]       ; sieb_liste[j] = 0
-        ADD  R5, R5, R4         ; j = j + i (nächstes Vielfaches)
-        B    while_02           ; zurück zur Pruefung der inneren Schleife
+        ADD  R5, R5, R4         ; j += i
+        B    while_02
 endwhile_02
-
 endif_01
-        ADD  R4, R4, #1         ; i = i + 1
-        B    while_01           ; zurück zur Pruefung der aeusseren Schleife
+        ADD  R4, R4, #1         ; i++
+        B    while_01
 endwhile_01
 
-        ; ============================================================
-        ; SCHRITT 3: ABSPEICHERN (wird noch nicht verlangt)
-        ; ============================================================
+        ; ===== SCHRITT 3: Abspeichern (noch nicht gefordert) =====
 
-        ; Programm soll nicht einfach irgendwo weiterlaufen
-        ; deshalb hängen wir es hier isn einer Endlosschleife auf
-forever B    forever
+forever B    forever           ; Endlosschleife
         ENDP
 
         ALIGN
