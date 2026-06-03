6840fe6f4d466b97eaa3aa8cd9993dd2b150f3df
arash <arash@MacBookPro.fritz.box>
2026-06-03 22:47:48 +0200

    Aufgabe 6 Primzahlsieb fertig

--- Programs/PrimzahlSiebASM/README.md
+++ Programs/PrimzahlSiebASM/README.md
@@ -1,30 +1,41 @@
 Primzahlsieb des Eratosthenes
 
 Beschreibung
-Sieb des Eratosthenes in ARM-Assembler. Findet alle Primzahlen von 2 bis 1000
-und markiert sie in sieb_liste (Index = Zahl, Wert 1 = Primzahl, 0 = keine).
+Sieb des Eratosthenes in ARM-Assembler. Findet alle Primzahlen von 2 bis 1000,
+markiert sie in sieb_liste (Index = Zahl, Wert 1 = Primzahl, 0 = keine) und
+speichert sie am Ende in ergebnis_liste ab.
 
 Ablauf
 
 Schritt 1 Initialisierung (for_01)
 Alle Einträge auf 1 setzen (erstmal alle als Primzahl annehmen).
-Index 0 und 1 danach auf 0, da keine Primzahlen.
+0 und 1 lassen wir aus, da keine Primzahlen.
 
 Schritt 2 Sieb (while_01 / while_02)
-- Äußere Schleife: i läuft ab 2, solange i*i größer gleich 1000
+- Äußere Schleife: i läuft ab 2, solange i*i kleiner gleich 1000
 - Ist sieb_liste[i] noch 1, werden alle Vielfachen von i (ab i*i) auf 0 gesetzt
 
-Schritt 3 Abspeichern
-Noch nicht gefordert. Speicher (ergebnis_liste) ist reserviert.
+Schritt 3 Abspeichern (for_02)
+Sieb von 2 bis 1000 durchgehen. Wo eine 1 steht, ist der Index eine Primzahl
+und wird mit STRH (2 Byte) in ergebnis_liste geschrieben. Offset danach um 2
+erhöhen. Am Ende eine 0 als Endemarkierung.
+
+Warum STRH
+1000 passt in 2 Byte, daher STRH statt STR (4 Byte). Das spart Platz. ALIGN vor
+ergebnis_liste, damit das Feld auf gerader Adresse beginnt.
 
 Register
- Reg / Bedeutung 
+ Reg / Bedeutung
 -----/-----------
-  R0 / Basisadresse sieb_liste 
-  R1 / i (Schritt 1) 
-  R4 / i (Schritt 2, äußere Schleife) 
-  R5 / j (innere Schleife) 
-  R6 / Hilfsregister 
+  R0 / Basisadresse sieb_liste
+  R1 / i (aktuelle Zahl)
+  R4 / i in Schritt 2, Basisadresse ergebnis_liste in Schritt 3
+  R5 / j in Schritt 2, Offset in Schritt 3
+  R6 / Hilfsregister
 
 Hinweis
-Vergleiche arbeiten im unsigned-Bereich (BHS / BHI), da alle Werte größer gleich 0 sind.
+Vergleiche arbeiten im unsigned-Bereich (BHS / BHI), da alle Werte groesser gleich 0 sind.
+
+Ergebnis prüfen im Watch-Fenster:
+*((uint16_t *)&ergebnis_liste)@200
+Zeigt 2, 3, 5, 7, 11 ... letzte ist 997, insgesamt 168 Primzahlen.
--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,9 +1,10 @@
 ;******************** (C) COPYRIGHT HAW-Hamburg ********************************
 ;* File Name          : PrimzahlSieb
 ;* Author             : Arash Ghaboos, Dennis Jarmolinski
-;* Version            : V3.0
-;* Date               : 27.05.2026
+;* Version            : V4.0
+;* Date               : 03.06.2026
 ;* Description        : Primzahlsieb des Eratosthenes für 2 bis 1000
+;*                      mit Abspeichern der Primzahlen in ein eigenes Feld
 ;*******************************************************************************
 
     EXTERN initITSboard
@@ -13,8 +14,12 @@
 ;============================================================
     AREA MyData, DATA, ALIGN=2
 
+    EXPORT ergebnis_liste       ; damit Watch-Fenster das Label erkennt
+
 sieb_liste      FILL 1001       ; Index 0..1000: 1 = Primzahl, 0 = keine
-ergebnis_liste  FILL 1000       ; für Schritt 3
+
+        ALIGN                   ; gerade Adresse, da wir 2-Byte STRH nutzen
+ergebnis_liste  FILL 400        ; Platz für die Primzahlen (je 2 Byte)
 
 ;============================================================
 ; Codesegment
@@ -24,16 +29,16 @@ ergebnis_liste  FILL 1000       ; für Schritt 3
     EXPORT main [CODE]
 
 main    PROC
-        BL   initITSboard       ; Board init
-        LDR  R0, =sieb_liste    ; R0 = Basisadresse
+        BL   initITSboard
+        LDR  R0, =sieb_liste    ; R0 = Basisadresse Sieb
 
         ; ===== SCHRITT 1: alle Einträge auf 1 setzen =====
-        
+
         MOV  R3, #1
 
 for_01
-        MOV  R1, #0             ; i = 0
-        MOV  R2, #1001        ; Endwert
+        MOV  R1, #2             ; i = 2 (0 und 1 sind keine Primzahlen)
+        MOV  R2, #1001          ; Endwert
 until_01
         CMP  R1, R2             ; i >= 1001? (unsigned)
         BHS  enddo_01           ; HS = Higher or Same (>=)
@@ -44,17 +49,14 @@ step_01
         B    until_01
 enddo_01
 
-        MOV  R3, #0             ; 0 und 1 sind keine Primzahlen
-        STRB R3, [R0, #0]
-        STRB R3, [R0, #1]
 
         ; ===== SCHRITT 2: Vielfache streichen =====
-        
+
         MOV  R4, #2             ; i = 2
 
 while_01
         MUL  R6, R4, R4         ; i*i
-        CMP  R6, #1000        ; i*i > 1000? (unsigned)
+        CMP  R6, #1000          ; i*i > 1000? (unsigned)
         BHI  endwhile_01        ; HI = Higher (>)
 do_while_01
 if_01
@@ -64,7 +66,7 @@ if_01
 then_01
         MUL  R5, R4, R4         ; j = i*i
 while_02
-        CMP  R5, #1000        ; j > 1000? (unsigned)
+        CMP  R5, #1000          ; j > 1000? (unsigned)
         BHI  endwhile_02        ; HI = Higher (>)
 do_while_02
         MOV  R6, #0
@@ -77,7 +79,36 @@ endif_01
         B    while_01
 endwhile_01
 
-        ; ===== SCHRITT 3: Abspeichern (noch nicht gefordert) =====
+
+        ; ===== SCHRITT 3: Primzahlen abspeichern =====
+        ; Sieb von 2 bis 1000 durchgehen. Wo eine 1 steht,
+        ; ist der Index eine Primzahl und wird gespeichert.
+
+        LDR  R4, =ergebnis_liste ; R4 = Basisadresse Ergebnisfeld
+        MOV  R5, #0             ; Offset im Ergebnisfeld
+
+for_02
+        MOV  R1, #2             ; i = 2
+        MOV  R2, #1001          ; Endwert
+until_02
+        CMP  R1, R2             ; i >= 1001? (unsigned)
+        BHS  enddo_02
+do_02
+if_02
+        LDRB R6, [R0, R1]       ; sieb_liste[i]
+        CMP  R6, #1             ; ist i eine Primzahl?
+        BNE  endif_02           ; nein -> nicht speichern
+then_02
+        STRH R1, [R4, R5]       ; Primzahl ablegen (2 Byte)
+        ADD  R5, R5, #2         ; Offset um 2 erhöhen
+endif_02
+step_02
+        ADD  R1, R1, #1         ; i++
+        B    until_02
+enddo_02
+
+        MOV  R6, #0             ; Endemarkierung
+        STRH R6, [R4, R5]       ; 0 hinter die letzte Primzahl
 
 forever B    forever            ; Endlosschleife
         ENDP
